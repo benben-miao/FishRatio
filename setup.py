847f4ff44a92dfc2161736be352601c3c71cd39f
benben-miao <benben.miao@outlook.com>
2021-08-05 00:00:59 +0800

    Add description to pypi home page

--- setup.py
+++ setup.py
@@ -3,15 +3,24 @@
 
 from setuptools import setup, find_packages
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+readme = 'fishratio/README.md'
+
 # https://pic3.zhimg.com/v2-4563e810d05ec25ce283c7f54a271e2e_r.jpg
 setup(
     name="fishratio",
-    version="1.0.0",
+    version="1.1.3",
     url="https://github.com/benben-miao/FishRatio",
     keywords=["CLI", "Biology", "Statistics"],
     description="Calculate the ratio and logarithmic value of species contained in several genus of a family to all species in this family.",
-    long_description="Calculate the ratio and logarithmic value of species contained in several genus of a family to all species in this family.",
-    download_url="https://pypi.python.org",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    download_url="https://pypi.org/project/fishratio",
 
     author="benben-miao",
     author_email="benben.miao@outlook.com",
@@ -31,9 +40,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
-    data_files=[
-        ('', ['README.md'])
-    ],
+    data_files=[readme],
     package_data={
         '':['examples/*.xlsx']
     },
